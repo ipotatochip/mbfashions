e0cef0bb3a88969c2b687a2dc17ee912b729edce
ipotatochip <akhilpati3@gmail.com>
2022-08-06 11:05:33 +0530

    updated

---
+++ js/jqueryscript.js
@@ -0,0 +1,11 @@
+/*MB Fashions Page jQuery Sheet*/
+
+
+/*********************/
+/*  01. Pre-Loader   */
+/*********************/
+
+$(window).on('load',function() {
+    $('#preloader_run').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
